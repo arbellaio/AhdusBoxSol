721e983df50dceb385aba2d15c49daa6f6c29153
Faizan Aamer <faizanaryan94@gmail.com>
2021-08-23 19:18:50 +0500

    Update AppConstants.cs

--- AhdusBoxSol/Resources/AppConstants.cs
+++ AhdusBoxSol/Resources/AppConstants.cs
@@ -3,12 +3,12 @@ namespace AhdusBoxSol.Resources
     public class AppConstants
     {
         public const string FirebaseDatabaseUrl =
-            "https://boxapp-87e47-default-rtdb.asia-southeast1.firebasedatabase.app/";
+            "";
 
-        public const string ApiKey = "AIzaSyB3royY6BTHzNxIHJJ1cFY3JuyOYNsNVYs";
-        public const string FirebaseFileStorageUrl = "boxapp-87e47.appspot.com";
-        public const string Email = "boxapp@gmail.com";
-        public const string Password = "Admin123.";
+        public const string ApiKey = "";
+        public const string FirebaseFileStorageUrl = "";
+        public const string Email = "";
+        public const string Password = "";
         
     }
 }
